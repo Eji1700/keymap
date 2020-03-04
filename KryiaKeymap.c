ccb0a367692e548563faae6653b6e5c315af6595
Eji1700 <regulus87@yahoo.com>
2020-03-03 21:21:55 -0800

    minor corrections to match current setup

--- KryiaKeymap.c
+++ KryiaKeymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       MT(MOD_LSFT, KC_TAB), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_CCCV,   XXXXXXX, XXXXXXX, KC_LEAD,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              LCTL(KC_BSPC), KC_LGUI, KC_LALT, LT(LOWER, KC_SPC), LT(RAISE, KC_DEL, LSFT(KC_ENT), LT(NAV, KC_SPC), MO(RAISE), KC_BSPC, LGUI(KC_TAB)
+              LCTL(KC_BSPC), KC_LGUI, KC_LALT, LT(LOWER, KC_SPC), LT(RAISE, KC_DEL), LSFT(KC_ENT), LT(RAISE, KC_SPC), MO(NAV), KC_BSPC, LGUI(KC_TAB)
     ),
 /*
  * Lower Layer: Numpad, Media
